f48f35da7ad095f85fc53d2b97fada27ed8cb83d
shinoyu <shinoyu.exp.develope@gmail.com>
2019-05-05 00:46:37 +0900

    add linux i3 settings.

---
+++ configs/linux_configs/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+local_script_dir=$(cd $(dirname $0); pwd) 
+local_target_dir=${XDG_CONFIG_HOME:-$HOME/.config}
+
+mkdir -p ${local_target_dir}/i3 && ln -sf ${local_script_dir}/i3.config ${local_target_dir}/i3/config
+mkdir -p ${local_target_dir}/i3status && ln -sf ${local_script_dir}/i3status.config ${local_target_dir}/i3status/config

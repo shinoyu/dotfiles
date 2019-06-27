87b9e3f118ad4b0689ae50e9b229d353046d5a7f
shinoyu <shinoyu.exp.develope@gmail.com>
2019-06-28 01:28:15 +0900

    load xmodmap on initialize.

--- configs/linux_configs/setup.sh
+++ configs/linux_configs/setup.sh
@@ -6,3 +6,5 @@ local_target_dir=${XDG_CONFIG_HOME:-$HOME/.config}
 
 mkdir -p ${local_target_dir}/i3 && ln -sf ${local_script_dir}/i3.config ${local_target_dir}/i3/config
 mkdir -p ${local_target_dir}/i3status && ln -sf ${local_script_dir}/i3status.config ${local_target_dir}/i3status/config
+ln -sf ${local_script_dir}/xmodmap $HOME/.xmodmap
+ln -sf ${local_script_dir}/xinitrc $HOME/.xinitrc

8bd4da0827d8bae46efbe70085c2caa004a07774
shinoyu <shinoyu.exp.develope@gmail.com>
2019-09-30 14:26:52 +0900

    apply tmux plugin system.

--- configs/tmux/setup.sh
+++ configs/tmux/setup.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
+set -e
+set -u
 
-: "setup tmux.conf" && { 
-	c_dir=$(cd $(dirname $0); pwd)
+: "settings tmux" && {
 
-	mkdir -p $HOME/.tmux
-	ln -sf  ${c_dir}/tmux.conf $HOME/.tmux.conf
-	ln -sf  ${c_dir}/tmux-$L_OSTYPE.conf $HOME/.tmux/tmux-$L_OSTYPE.conf
+c_dir=$(cd $(dirname $0); pwd)
+init_tmux() {
+	mkdir -p $HOME/.tmux/plugins
+}
+setup_conf() {
+	ln -sf  $c_dir/tmux.conf $HOME/.tmux.conf
+	ln -sf  $c_dir/tmux-$L_OSTYPE.conf $HOME/.tmux/tmux-$L_OSTYPE.conf
+	ln -sf  $c_dir/plugins.conf $HOME/.tmux/plugins.conf
+}
+
+install_plugin_packages() {
+	if type git >/dev/null; then
+		if ! test -e $TMUX_PLUGIN_MANAGER_PATH/tpm; then
+			git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_MANAGER_PATH/tpm
+		fi
+	else
+		echo "warning!! require git. skip ====>"
+	fi
+}
+
+echo "== run tmux settings.... ===================\n"
+init_tmux
+setup_conf
+echo "== install plugin manager.... ==============\n"
+install_plugin_packages
+echo "== finish tmux settings!! ==================\n"
+
 }

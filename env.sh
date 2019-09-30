f50ce1a748d65299d44002ece81ba71dcc839fbb
shinoyu <shinoyu.exp.develope@gmail.com>
2019-09-30 11:19:33 +0900

    Difinition of fixed ENV by env.sh && rename mitamae script, do not call mitamae in init.sh.

---
+++ env.sh
@@ -0,0 +1,7 @@
+: "set env args" && {
+	uname -a | grep "microsoft" | grep "Linux">/dev/null && if [ $? -eq 0 ]; then
+		export L_OSTYPE="Windows_Linux"
+	else
+		export L_OSTYPE=`uname`
+	fi
+}
--- init.sh
+++ init.sh
@@ -9,10 +9,12 @@ cd dotfiles
 		sudo chown -R ${USER}:${USER} .
 	fi
 }
-sh ./post_init
 
 : "os_init" && {
 	source ./init/init_darwin.sh
 	source ./init/init_brew.sh
 }
+
+#sh ./install_mitamae.sh
+sh ./env.sh
 sh ./install.sh

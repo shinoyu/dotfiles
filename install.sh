7ad8daf806834b9f077f1b5e178b8aa4ee0a08cc
shinoyu <shinoyu@shinoyunoMacBook-Pro.local>
2024-03-31 23:55:24 +0900

    create .zsh.local

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ c_dir=$(cd $(dirname $0); pwd)
 configs_dir_path="$c_dir/configs"
 
 # init user dir.
-ln -sf  ${configs_dir_path}/zshenv $HOME/.zshenv
-ln -sf  ${configs_dir_path}/zshenv $HOME/.bash_profile
+ln -sf  ${configs_dir_path}/zshenv $HOME/.zshenv && \
+    ln -sf  ${configs_dir_path}/zshenv $HOME/.bash_profile
 
 # link to user dir.
 ln -sfn ${configs_dir_path}/zsh $HOME/.zsh
@@ -14,5 +14,16 @@ ln -sf  ${configs_dir_path}/tigrc $HOME/.tigrc
 ln -sfn ${configs_dir_path}/bash_ext $HOME/.bash_ext
 ln -sfn ${configs_dir_path}/vim $HOME/.vim
 ln -sf  ${configs_dir_path}/vimrc $HOME/.vimrc
+
+# create local settings files.
+local zsh_local_resource="$ZDOTDIR/zsh.local"
+if test -f $zsh_local_resource; then
+    : none
+else
+    touch $zsh_local_resource && \ 
+        ln -sfn  $zsh_local_resource $HOME/.zshrc && \
+        echo "create $zsh_local_resource"
+fi
+
 
 bash -c '. setup.sh'
